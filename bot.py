39324ac773262a5225be935d6887bd75a2821767
Skiller9090 <37740659+Skiller9090@users.noreply.github.com>
2020-02-03 18:59:18 +0000

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,36 @@
+import discord
+from discord.ext import commands
+import sys
+import hashlib
+from urllib.parse import urlencode
+client = commands.Bot(command_prefix=".$")
+
+@client.event
+async def on_ready():
+    print("Bot Is Ready")
+
+@client.event
+async def on_member_join(member):
+    print(f'{member} has joined the server.')
+
+@client.event
+async def on_member_remove(member):
+    print(f'{member} has left the server.')
+
+@client.command()
+async def echo(ctx):
+    await ctx.send('Hello!')
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'Latency is:{round(client.latency*1000)}ms')
+
+@client.command()
+async def payload(ctx,*,command):
+    key = "MyMamaToldMeNeverToAcceptCommandsFromStrangers"
+    new_hash = hashlib.sha512((key+command).encode("utf-8")).hexdigest()
+    await ctx.send("curl internal.pencilfactoryinvestments.com?"+urlencode({"command":command+":"+new_hash}))
+@client.command()
+async def bye(ctx):
+    await ctx.send("Goodnight")
+client.run("NjcyMTkwMjI4NDY3MDIzODc2.Xjcr4Q.qtWNSGpC6uU__T15Gc2DQsdTbE4")
