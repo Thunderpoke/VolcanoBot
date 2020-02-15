01e8cadde44f363196c6246ed9766d38b49cb6e8
Skiller9090 <37740659+Skiller9090@users.noreply.github.com>
2020-02-15 19:05:24 +0000

    Cogs Done

--- bot.py
+++ bot.py
@@ -12,45 +12,8 @@ client = commands.Bot(command_prefix="$")
 client.remove_command("help")
 
 
-
-@client.event
-async def on_ready():
-    print("Bot Is Ready")
-
-
-@client.event
-async def on_member_join(member):
-    client.get_channel(658064237112983554).send(f'{member} just joined the server. Why is he allowed here?')
-
-
-@client.event
-async def on_member_remove(member):
-    client.get_channel(658064237112983554).send(f'Well, {member} just left the server. No more Volcano flags for him.')
-
-
-@client.command()
-async def echo(ctx, *, message):
-    if message == "":
-        await ctx.send(f'Hello {ctx.author}!')
-    else:
-        await ctx.send(message)
-
-
-@client.command()
-async def ping(ctx):
-    await ctx.send(f'Latency is: {round(client.latency*1000)}ms')
-
-
-@client.command()
-async def bye(ctx):
-    await ctx.send("Goodnight")
-
-
-@client.command()
-async def anger(ctx):
-    await ctx.send("Well that's a f***ing pain in the arse")
-
-
+client.load_extension("cogs.fun")
+client.load_extension("cogs.events")
 client.load_extension("cogs.help")
 
 @client.command()
@@ -58,4 +21,4 @@ async def purge(ctx, amount=1):
     if ctx.author.id == 525005875098812416:
         await ctx.channel.purge(limit=(amount + 1))
 
-client.run("NjcyMTkwMjI4NDY3MDIzODc2.Xkg3Vw.OA28IE7WigmtocvTEadXYgME7gM")
+client.run("NjcyMTkwMjI4NDY3MDIzODc2.Xkg6tA.ilPi1A5ELTgboahy8AeR0v5obfo")
---
+++ cogs/events.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+
+
+class mainEvents(commands.Cog):
+    def __init__(self,bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Bot Is Ready")
+
+    @commands.Cog.listener()
+    async def on_member_join(self,member):
+        self.bot.get_channel(658064237112983554).send(f'{member} just joined the server. Why is he allowed here?')
+    
+    @commands.Cog.listener()
+    async def on_member_remove(self,member):
+        self.bot.get_channel(658064237112983554).send(f'Well, {member} just left the server. No more Volcano flags for him.')
+
+
+def setup(bot):
+    bot.add_cog(mainEvents(bot))
