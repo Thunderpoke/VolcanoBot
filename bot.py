c78823f227391f69cb2fdc36fe79b1b907f98880
Bob Bobety <andrej.ljubic05@hotmail.com>
2020-02-15 16:39:55 +0000

    Removed Cog due to Complexity
    Cog was removed due to too much complexity for current purpose, help command was redefined elsewhere for now

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import discord
 from discord.ext import commands
 import logging
 
@@ -10,34 +11,51 @@ logger.addHandler(handler)
 client = commands.Bot(command_prefix="$")
 client.remove_command("help")
 
+
 @client.event
 async def on_ready():
     print("Bot Is Ready")
 
+
 @client.event
 async def on_member_join(member):
-    print(f'{member} has joined the server.')
+    print(f'{member} just joined the server. Why is he allowed here?')
+
 
 @client.event
 async def on_member_remove(member):
-    print(f'{member} has left the server.')
+    print(f'Well, {member} just left the server. No more Volcano flags for him.')
+
 
 @client.command()
 async def echo(ctx):
-    await ctx.send('Hello!')
+    await ctx.send(f'Hello {ctx.author}!')
+
 
 @client.command()
 async def ping(ctx):
     await ctx.send(f'Latency is: {round(client.latency*1000)}ms')
 
+
 @client.command()
 async def bye(ctx):
     await ctx.send("Goodnight")
+
 
 @client.command()
 async def anger(ctx):
     await ctx.send("Well that's a f***ing pain in the arse")
 
-client.load_extension("cogs.help")
 
-client.run("NjcyMTkwMjI4NDY3MDIzODc2.Xkfr_g.I3fSZBTXboGSZmAWjy8Lt2OU4KQ")
+@client.command("help")
+async def _help(ctx):
+    heading = discord.Embed(title='Help',
+        description=
+            """Hiya there! Guess you need help...""")
+
+    await ctx.message.add_reaction(emoji='✉')
+    await ctx.message.author.send('', embed=heading)
+
+# client.load_extension("cogs.help")
+
+client.run("NjU2MTg1MTc4MDIwMTE4NTUx.XkgZnw.p7bDCiRWnj0LGKyOL3pXrb3mob4")
